c596c5559a5b644e02726284aeeb277d6cfa4acc
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-07-13 10:56:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  
+
+  return (
+    <div className=" h-screen w-screen">
+      <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5">
+        <a href="/create">Add New Product</a>
+        <hr className="w-[80%]"></hr>
+        <h1>Category</h1>
+        <ul>
+          <li>
+      cat1
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+export default App;
